e82e6381dd2b06c8efb20876653712ea0eef948b
Tom <tomn98@live.unc.edu>
2020-10-31 19:25:56 -0400

    Added HammerJS

--- js/main.js
+++ js/main.js
@@ -10,4 +10,18 @@ $(function() {
         $(this).addClass("active");
         click_sfx.play();
     })
+
+
+    var clickwheel = document.getElementById("clickwheel_container");
+    var cw = new Hammer(clickwheel);
+
+    cw.get("pan").set({ direction: Hammer.DIRECTION_ALL });
+
+    cw.on("panleft panright panup pandown tap press", function(ev) {
+        console.log( ev.center.x );
+
+        $("#cw_touch").css("left", ev.center.x + "px");
+        $("#cw_touch").css("top", ev.center.y + "px")
+    });
+
 })
