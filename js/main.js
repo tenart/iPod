94f4acbedf7e18a84aa2f2b231218754559ef83e
Tom <tomn98@live.unc.edu>
2020-11-01 13:01:38 -0500

    Improved touch recognition

--- js/main.js
+++ js/main.js
@@ -18,10 +18,19 @@ $(function() {
     cw.get("pan").set({ direction: Hammer.DIRECTION_ALL });
 
     cw.on("panleft panright panup pandown tap press", function(ev) {
-        console.log( ev.center.x );
 
-        $("#cw_touch").css("left", ev.center.x + "px");
-        $("#cw_touch").css("top", ev.center.y + "px")
+        var offset = {
+            x: parseFloat($("#cw_outer").css("left")),
+            y: parseFloat($("#cw_outer").css("top"))
+        }
+
+        $("#cw_touch").css("opacity", 1);
+        $("#cw_touch").css("margin-left", (ev.center.x - offset.x) + "px");
+        $("#cw_touch").css("margin-top", (ev.center.y - offset.y) + "px");
+
+        if( ev.isFinal ) {
+            $("#cw_touch").css("opacity", 0);
+        }
     });
 
 })
