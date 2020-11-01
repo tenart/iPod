2a2519f59dbbc43ea7b036e72df770789cc5cc90
Tom <tomn98@live.unc.edu>
2020-11-01 14:31:29 -0500

    Added direction detection

--- js/main.js
+++ js/main.js
@@ -12,44 +12,62 @@ $(function() {
     })
 
 
-    var clickwheel = document.getElementById("clickwheel_container");
-    var cw = new Hammer(clickwheel);
+    var clickwheel_container = document.getElementById("clickwheel_container");
+    var cwHammer = new Hammer(clickwheel_container);
+    var clickwheel = {
+        x: 0,
+        y: 0,
+        angle: 0,
+        direction: ""
+    }
 
-    cw.get("pan").set({ direction: Hammer.DIRECTION_ALL });
-
-    cw.on("panleft panright panup pandown tap press", function(ev) {
-
-        var touch = {
-            x: ev.center.x - parseInt($("#cw_outer").css("left")),
-            y: ev.center.y - parseInt($("#cw_outer").css("top"))
-        }
+    cwHammer.get("pan").set({ direction: Hammer.DIRECTION_ALL });
+    cwHammer.on("panleft panright panup pandown tap press", function(ev) {
 
         var center = {
             x: 146,
             y:146
         }
 
-        var angle = parseInt(angleDeg(touch, center));
+        clickwheel.x = ev.center.x - parseInt($("#cw_outer").css("left"));
+        clickwheel.y = ev.center.y - parseInt($("#cw_outer").css("top"));
 
+        var newAngle = parseInt(anglePoints(clickwheel, center)) + 179;
+
+        var overRev = Math.abs((newAngle - clickwheel.angle)) >= 90;
+
+        if( newAngle > clickwheel.angle ) {
+            if (!overRev) clickwheel.direction = ">>";
+        } else if( newAngle < clickwheel.angle ) {
+            if (!overRev) clickwheel.direction = "<<";
+        }
+
+        clickwheel.angle = parseInt(anglePoints(clickwheel, center)) + 179;
+
+        $("#cw_touch").css("left", clickwheel.x + "px");
+        $("#cw_touch").css("top", clickwheel.y + "px");
+        $("#cw_needle").css("transform", "rotate(" + (clickwheel.angle - 179) + "deg)");
+
+        $("#cw_x span").text( clickwheel.x );
+        $("#cw_y span").text( clickwheel.y );
+        $("#cw_angle span").text( (clickwheel.angle) );
+        $("#cw_direction span").text( (clickwheel.direction) );
+    });
+
+    cwHammer.on("panstart", function(ev) {
         $("#cw_touch").css("opacity", 1);
         $("#cw_needle").css("opacity", 1);
+    })
 
-        $("#cw_touch").css("left", (touch.x) + "px");
-        $("#cw_touch").css("top", (touch.y) + "px");
-        $("#cw_needle").css("transform", "rotate(" + angle + "deg)");
-
-        $("#cw_x span").text( (touch.x) + "px" );
-        $("#cw_y span").text( (touch.y) + "px" );
-        $("#cw_angle span").text( angle );
-
-        if( ev.isFinal ) {
-            $("#cw_touch").css("opacity", 0);
-            $("#cw_needle").css("opacity", 0);
-        }
-    });
+    cwHammer.on("panend pancancel", function(ev) {
+        $("#cw_touch").css("opacity", 0);
+        $("#cw_needle").css("opacity", 0);
+        clickwheel.direction = "-";
+        $("#cw_direction span").text( (clickwheel.direction) );
+    })
 
 })
 
-function angleDeg(p1, p2) {
+function anglePoints(p1, p2) {
     return Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
 }
